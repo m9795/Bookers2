5b8875d9d3e460116237245b981cdfbf4f353b8b
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-09 19:58:53 +0900

    [Add] 4-25 Model アソシエーションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about' as: 'about'
+  get 'homes/about' => 'homes#about', as: 'about'
   
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
