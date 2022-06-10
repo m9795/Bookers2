fbdba73e52f7301c8c52e661f81974936ce5f84e
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-10 16:55:35 +0900

    [Add] 4-25 books indexページ項目おおまかに入力

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  end
+
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:user_id, :title, :body)
   end
 end
