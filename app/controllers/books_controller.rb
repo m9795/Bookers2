624f00a0fd1f137dc6b0ef67d6953ce9af412abe
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-10 17:16:03 +0900

    [Add] 4-25 book投稿機能、保存機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
   
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
@@ -18,6 +23,6 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.require(:book).permit(:user_id, :title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
