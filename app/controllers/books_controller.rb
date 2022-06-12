a2236b89efc936c1147613e3e87ce6904ce9592b
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-12 23:27:13 +0900

    [Add] 4-25 book新規投稿　バリデーション設定,エラー文の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
     end
   end
 
@@ -32,7 +36,7 @@ class BooksController < ApplicationController
       redirect_to book_path(book.id)
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
