35f2465bf23e01020743b8de688d5fea2c3ddb1c
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-12 15:19:23 +0900

    [Update] 4-25 コントローラの変更 ユーザー画面から投稿できるようになった

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,9 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
     redirect_to books_path
   end
 
